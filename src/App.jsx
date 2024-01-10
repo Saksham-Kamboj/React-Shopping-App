e649fc475acecb99962e0c9fc0396fdaa0b9bcbf
SAKSHAM KAMBOJ <sakshamkamboj2072@gmail.com>
2024-01-10 07:09:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import MenProducts from "./components/Men/Men";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import WomenProducts from "./components/Women/Women";
+import ChildProducts from "./components/Child/Child";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
+import Popup from "./components/Popup/Popup";
+
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <MenProducts />
+      <WomenProducts handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <Subscribe />
+      <ChildProducts />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  );
+};
+
+export default App;
