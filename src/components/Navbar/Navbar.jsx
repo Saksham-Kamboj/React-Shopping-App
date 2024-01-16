dd1e79049fb79416306523349d8d767bc750456f
SAKSHAM KAMBOJ <sakshamkamboj2072@gmail.com>
2024-01-16 13:46:58 +0530

    Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,11 +26,11 @@ const Menu = [
     name: "Mens Wear",
     link: "/#",
   },
-  {
-    id: 3,
-    name: "Electronics",
-    link: "/#",
-  },
+  // {
+  //   id: 3,
+  //   name: "Electronics",
+  //   link: "/#",
+  // },
 ];
 
 const DropdownLinks = [
